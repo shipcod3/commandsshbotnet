8f36334361253e13e7effe1609ceba83c1c4ea00
Jay Turla <shipcodez@gmail.com>
2014-08-26 16:45:11 +0800

    Update test_pxssh.py

--- test_pxssh.py
+++ test_pxssh.py
@@ -11,15 +11,9 @@ try:
     username = raw_input('SET USERNAME: ')
     password = getpass.getpass('SET PASSWORD: ')
     s.login (hostname, username, password)
-    s.sendline ('uptime')   # run a command
+    s.sendline ('uname -a')   # run a command
     s.prompt()             # match the prompt
     print s.before          # print everything before the prompt.
-    s.sendline ('ls -l')
-    s.prompt()
-    print s.before
-    s.sendline ('df')
-    s.prompt()
-    print s.before
     s.logout()
 except pxssh.ExceptionPxssh, e:
     print "pxssh failed on login."
