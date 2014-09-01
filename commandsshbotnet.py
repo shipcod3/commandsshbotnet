5d44d98a2ec0693056c3ea7795ec1b025f3ce6ee
Jay Turla <shipcodez@gmail.com>
2014-09-01 09:30:06 +0800

    Update commandsshbotnet.py
    adding more command options

--- commandsshbotnet.py
+++ commandsshbotnet.py
@@ -1,9 +1,11 @@
 #!usr/bin/env python
 
 ##########################################################
+#                                                        #
 # commandsshbotnet.py                                    #
 # author: @shipcod3                                      #
 # inspired by the mass ssh botnet in " Violent Python "  #
+#                                                        #
 ##########################################################
 
 import optparse
@@ -33,13 +35,19 @@ class Client:
         return self.session.before # print everything before the prompt
 
 def usage():
-    print "Sample Usage: python commandsshbotnet.py <command>"
+    print """
+    *********
+    Commands:
+    *********
+    python commandsshbotnet.py os <command> - For control"
+    python commandsshbotnet.py info - Print information"
+    """
 
 def main(argv):
     if len(argv) < 2:
         return usage()
 
-    os_command = sys.argv[1]
+    arg_command = sys.argv[1]
 
     def botnetCommand(command):
         for client in botNet:
@@ -47,15 +55,30 @@ def main(argv):
             print '[*] IP: ' + client.host
             print '[+] Command: ' + output
 
-    def addConfig(host, user, password):
+    def info(command):
+        for client in botNet:
+            output = client.send_command(command)
+            print '[*] IP: ' + client.host
+            print output.strip('uname -a;uptime')
+
+    def addClient(host, user, password):
         client = Client(host, user, password)
         botNet.append(client)
 
     botNet = []
     #add your host, user, and password here 
-    addConfig('127.0.0.1', 'celso', 'celso123') #sample config
-    addConfig('127.0.0.2', 'celso', 'celso123')
-    botnetCommand(os_command)
+    addClient('127.0.0.1', 'celso', 'celso123') #sample config
+    addClient('127.0.0.2', 'celso', 'celso123')
+
+    if arg_command == "os":
+        os_command = sys.argv[2]
+        botnetCommand(os_command)
+        
+    elif arg_command == "info":
+        info("uname -a;uptime")
+
+    else:
+        return usage()
 
 if __name__ == "__main__":
     main(sys.argv)
